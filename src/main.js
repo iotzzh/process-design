42df97dab7f38a343f983c33bc4355f4df97325e
zhangzhonghua <956159241@qq.com>
2023-06-09 17:07:40 +0800

    添加BPMNjs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import './assets/main.css'
+
+// bpmn.js
+import 'bpmn-js/dist/assets/diagram-js.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
+// 左边工具栏以及编辑节点的样式
+import 'bpmn-js-properties-panel/dist/assets/element-templates.css'; // 工具栏样式
+import 'bpmn-js-properties-panel/dist/assets/properties-panel.css'; // 工具栏样式
+
 import "@logicflow/core/dist/style/index.css";
 import "@logicflow/extension/lib/style/index.css";
 
--- src/views/BPMN/index.vue
+++ src/views/BPMN/index.vue
@@ -1,21 +1,64 @@
 <template>
-    BPMN
+    <div id="app">
+        <div class="container" style="width: 100%; height: 100%;">
+            <!-- 创建一个canvas画布 npmn-js是通过canvas实现绘图的，并设置ref让vue获取到element -->
+            <div class="bpmn-canvas" ref="canvas" style="width: 100%; height: 100%;"></div>
+        </div>
+    </div>
 </template>
 
 <script setup>
+import { template as bpmnTemplate } from './static'
+import BpmnModeler from "bpmn-js/lib/Modeler";
+import { onMounted, ref } from 'vue';
 
+const bpmnModeler = ref();
+const canvas = ref();
 
-const setData = function (data) {
-//   lf.value.render(data);
+async function createNewDiagram(bpmn) {
+    // 将字符串转换成图显示出来;
+    bpmnModeler.value.importXML(bpmn, err => {
+        if (err) {
+            this.$Message.error("打开模型出错,请确认该模型符合Bpmn2.0规范");
+        }
+    });
 }
 
-const getData = function (data) {
-//   return lf.value.getGraphData();
+
+// 初始化方法
+async function init() {
+    // // 获取画布 element
+    // const canvas = canvas;
+
+    // 创建Bpmn对象
+    bpmnModeler.value = new BpmnModeler({
+        // 设置bpmn的绘图容器
+        container: canvas.value,
+    });
+
+    await createNewDiagram(bpmnTemplate); // 将图转换成图显示出来
+}
+
+
+onMounted(() => {
+    init();
+});
+
+const setData = function (data) {
+    createNewDiagram(data);
+}
+
+const getData = async function (callback) {
+    await bpmnModeler.value.saveXML({ format: true }, (err, xml) => {
+        if (!err) {
+            callback(xml);
+        }
+    });
 }
 
 
 defineExpose({
-  setData,
-  getData,
+    setData,
+    getData,
 });
 </script>
